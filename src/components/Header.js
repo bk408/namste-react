4ca1e67a5c75622fd3f38ae0d3cdfea90b282f70
Bhavya Khatri <bhavyakhatri1912@gmail.com>
2023-08-28 08:12:58 +0530

    episode-9 online status created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constant";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
   let [btnNameReact, setbtnNameReact] = useState("Login");
+
+  const OnlineStatus = useOnlineStatus();
 
   return (
     <div className="header">
@@ -12,6 +15,7 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
+          <li>Online Staus: {OnlineStatus ? "🟢" : "🔴"} </li>
           <li>
             <Link to="/">Home</Link>
           </li>
