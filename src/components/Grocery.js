b0dd6500751d58fedab2649dfb62409bae8108e5
Bhavya Khatri <bhavyakhatri1912@gmail.com>
2023-08-29 21:59:26 +0530

    episode-9 completed & Grocery component created

---
+++ src/components/Grocery.js
@@ -0,0 +1,10 @@
+
+
+
+const Grocery = () => {
+    return(
+         <h1>Welcome to Grocery store. You can find here fresh grocery items in less price !!! 🍉🍊🥑🥘🍅🍟🍞🍕🥗🥜🥝🍪</h1>
+    )
+}
+
+export default Grocery;
