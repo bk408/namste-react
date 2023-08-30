d5787343fed1a38c18f271e9ece269cc34eac50c
Bhavya Khatri <bhavyakhatri1912@gmail.com>
2023-08-30 16:39:35 +0530

    episode 10 completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { LOGO_URL } from "../utils/constant";
+import { FOOD_URL } from "../utils/constant";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
 
@@ -9,28 +9,30 @@ const Header = () => {
   const OnlineStatus = useOnlineStatus();
 
   return (
-    <div className="header">
+    <div className="flex justify-between shadow-lg bg-green-50">
       <div className="logo-container">
-        <img className="logo" src={LOGO_URL} />
+        <img className="h-14 px-4 mt-3" src={FOOD_URL} />
       </div>
-      <div className="nav-items">
-        <ul>
-          <li>Online Staus: {OnlineStatus ? "🟢" : "🔴"} </li>
-          <li>
+      <div className="flex items-center">
+        <ul className="flex p-4 m-4">
+          <li className="px-4 text-lg">
+            Online Staus: {OnlineStatus ? "🟢" : "🔴"}{" "}
+          </li>
+          <li className="px-4 text-lg">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="px-4 text-lg">
             <Link to="/about">About Us</Link>
           </li>
-          <li>
+          <li className="px-4 text-lg">
             <Link to="/contact">Contact US</Link>
           </li>
-          <li>
+          <li className="px-4 text-lg">
             <Link to="/grocery">Grocery</Link>
           </li>
-          <li>Cart</li>
+          <li className="px-4 text-lg">Cart</li>
           <button
-            className="login"
+            className="login text-lg px-4"
             onClick={() => {
               btnNameReact === "Login"
                 ? setbtnNameReact("Logout")
